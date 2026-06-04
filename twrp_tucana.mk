3bc6e2a44e44e41f03862dd7f2c3734356433977
LIECOTI <ib6778794@gmail.com>
2026-06-04 17:00:58 +0300

    Update twrp_tucana.mk

--- twrp_tucana.mk
+++ twrp_tucana.mk
@@ -1,13 +1,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-$(call inherit-product, bootable/recovery/twrp_product.mk)
+$(call inherit-product, vendor/ot/config/fox.mk)
 
 PRODUCT_DEVICE := tucana
 PRODUCT_NAME := twrp_tucana
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_RELEASE_NAME := tucana
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_RELEASE_NAME := Xiaomi Mi Note 10
